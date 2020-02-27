f689c12be7fc428c0baf7dc77a298f85763fe7a4
dpgian <dpgian@gmail.com>
2020-02-27 20:28:36 +0000

    second commit

---
+++ config/index.js
@@ -0,0 +1,6 @@
+const DBUSER = 'gianExp'
+const DBPASSWORD = 'Exp1r3dPass1'
+
+const MONGODB_URI = `mongodb+srv://${DBUSER}:${DBPASSWORD}@expy-cluster-n219t.mongodb.net/test?retryWrites=true&w=majority`
+
+module.exports = MONGODB_URI
