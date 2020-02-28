ac9706109eafdec6b17b24374dfb6ebf29f53efe
dpgian <dpgian@gmail.com>
2020-02-28 18:49:46 +0000

    Added .env and removed config folder

--- models/index.js
+++ models/index.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
-const URI = require('../config/index')
+const {parsed: {DBUSER}} = require('dotenv').config()
+const {parsed: {DBPASSWORD}} = require('dotenv').config()
+const URI = `mongodb+srv://${DBUSER}:${DBPASSWORD}@expy-cluster-n219t.mongodb.net/test?retryWrites=true&w=majority`
 
 mongoose.connect(process.env.MONGODB_URI || URI, { useNewUrlParser: true, useUnifiedTopology: true})
 
