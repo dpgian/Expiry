60b748f8fc1f5a3b6c3800b48321d8ca30e3fde6
dpgian <dpgian@gmail.com>
2020-03-05 21:02:45 +0000

    Implemented simple Add and Delete API calls

--- client/src/pages/Items.js
+++ client/src/pages/Items.js
@@ -3,87 +3,13 @@ import API from '../utils/API'
 import MaterialTable, { MTableFilterRow, MTableBody, MTableToolbar } from 'material-table'
 
 // class Item extends Component {
-//     state = {
-//         items: [],
-//         name: '',
-//         date: '',
-//         store: '',
-//         search: ''
-//     }
-
-//     componentDidMount() {
-//         this.loadItems()
-//     }
-
-//     loadItems = () => {
-//         API.getItems()
-//             .then(res => this.setState({ items: res.data, name: '', date: '', store: res.data[0].store}))
-//             .catch(err => console.log(err))
-        
-//     }
-
+//     
 //     deleteBook = id => {
 //         API.deleteItem(id)
 //             .then(res => this.loadItems())
 //             .catch(err => console.log(err))
 //     }
 
-//     handleInputChange = e => {
-//         let { name, value } = e.target
-//         this.setState({
-//             [name]: value
-//         })
-//     }
-
-//     handleFormSubmit = e => {
-//         e.preventDefault()
-//         if (this.state.name && this.state.date) {
-//             API.saveItem({
-//                 name: this.state.name,
-//                 date: this.state.date
-//             })
-//                 .then(res => this.loadItems())
-//                 .catch(err => console.log(err))
-//         }
-//     }
-
-//     render() {
-//         return (
-//             <>
-//             {/* <input type='text' placeholder='Search..' name='search' value={this.state.search} onChange={this.handleInputChange} />
-//             <table style={{textAlign: 'center'}}>
-//                 <thead>
-//                     <tr>
-//                         <th onClick={() => console.log(this.state.display)}>Name</th>
-//                         <th>Exp</th>
-//                         <th></th>
-//                     </tr>
-//                 </thead>
-//                 <tbody>
-//                     { this.state.items.filter((item) => 
-//             item.name.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1
-//         ).map(item => 
-//                                 <tr key={item.name}> 
-//                                 <td>{item.name}</td>
-//                                 <td>{item.date}</td>
-//                                 <td>
-//                                     <button onClick={()=>console.log('remove '+item._id)}>Remove</button>                                                   
-//                                 </td>
-//                                 </tr>)
-//                                 }
-//                 </tbody>
-//             </table>
-//              */}
-
-//             Hello world.
-
-            
-
-//             </>
-//            )
-//     }
-
-// }
 
 function Item() {
 
@@ -105,12 +31,20 @@ function Item() {
                 : 
                 newarray = [...newarray, {...x, date: x.date[0]}]
             })
-        
+
         } 
-        console.log(newarray)
+        
         setData(newarray)
+        //console.log(newarray)
+        setStore(newarray[0].store)
     }
   
+    async function fetchData() {
+      API.getItems()
+             .then(res => splitData(res.data))
+             .catch(err => console.log(err))
+    }
+
     // Enforces sort on date
     let sortDate = () => {
       let a = data.sort(function(a, b){
@@ -122,12 +56,11 @@ function Item() {
     }
   
     useEffect(() => {
-        API.getItems()
-             .then(res => splitData(res.data))
-             .catch(err => console.log(err))
+        fetchData()
     }, [])
 
     let [ data, setData ] = useState([])
+    let [ store, setStore ] = useState('')
 
     return (
       <>
@@ -139,22 +72,17 @@ function Item() {
             ]}
             data={data}
             editable={{
-              onRowAdd: newItem => new Promise((res, rej) => {
-                setTimeout(() => {
-                  setData([...data, newItem])
-                  res()
-                }, 1000)
-              }),
-              //onRowDelete: (item) => Promise.resolve(console.log(item))
-              onRowDelete: oldItem => new Promise((res, rej) => {
-                setTimeout(() => {
-                  let i = data.indexOf(oldItem)
-                  let temp = data
-                  temp.splice(i, 1)
-                  setData([...temp])
-                  res()
-                }, 1000)
-              })
+              onRowAdd: newItem => API.saveItem({
+                                        store,
+                                        name: newItem.name,
+                                        date: newItem.date
+                                      })
+                                      .then(res => fetchData())
+                                      .catch(err => console.log(err))
+              ,
+              onRowDelete: oldItem => API.deleteItem(oldItem._id)
+                                         .then(res => fetchData())
+                                         .catch(err => console.log(err))
             }}
             options={{
               actionsColumnIndex: -1,
